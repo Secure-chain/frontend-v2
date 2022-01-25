c6bfe1b15252ed5bc251cd1a541b83711e841e5c
raghusodani <201951118@iiitvadodara.ac.in>
2022-01-25 19:24:16 +0530

    supply chain details page done

---
+++ src/APIcalls/CreateSupplyChain/postSupplyChainDetails.js
@@ -0,0 +1,16 @@
+import { initObject } from "../../components/initVariables/initObject"
+import axios from "axios"
+export const postSupplyChainDetails = async(data) => {
+     try{
+        let res = await axios.post(initObject().initVariables+"/supplychain/",data,{
+            headers: {
+                Authorization: `Token ${initObject().token}`,
+            }
+        })
+        // console.log('api response 🚀', res)
+        return res.data.id
+     }
+     catch(error){
+        console.error(error)
+     }
+}
--- src/components/createSupplyChain.js/SupplyChainDetails.js
+++ src/components/createSupplyChain.js/SupplyChainDetails.js
@@ -1,9 +1,53 @@
-import React from 'react';
-
-function SupplyChainDetails() {
+import React, { useState } from 'react';
+import Input from '../common/input/Input'
+import Button from '../common/button/Button';
+import './supplyChainDetails.css';
+import { initObject } from '../initVariables/initObject';
+import { postSupplyChainDetails } from '../../APIcalls/CreateSupplyChain/postSupplyChainDetails';
+function SupplyChainDetails({setId}) {
+  const [supplyChainDetails, setSupplyChainDetails] = useState({
+    name: '',
+    details: '',
+  });
+  const handleNext = async() => {
+    const ids =  await postSupplyChainDetails(supplyChainDetails);
+    setId(ids);
+  }
   return (
   <div>
-    Helllo from SupplyChainDetails
+    <div className='supply-chain-container'>
+      <div className='supply-chain-header'>
+        Supply Chain Details
+      </div>
+      <div className='input-container'>
+        <div className='input-label'>
+          Name
+        </div>
+        <Input 
+          type={'text'} 
+          value={supplyChainDetails.name} 
+          onChange={(e)=>{
+          setSupplyChainDetails({...supplyChainDetails, name: e.target.value})
+          }}
+          style={{width: "100%",}} 
+        />
+        <div className='input-label'>
+          Description
+        </div>
+        <Input 
+          type={'text'}
+          value={supplyChainDetails.description}
+          onChange={(e)=>{
+            setSupplyChainDetails({...supplyChainDetails, details: e.target.value})
+          }
+          }
+          style={{width: "100%", height: "100px",marginBottom: "20px"}}
+        />
+      </div>
+      <div className={'next-btn'}>
+        <Button text={"Next Page"}  onClick={handleNext} />
+      </div>     
+    </div>
   </div>
   )
   ;
