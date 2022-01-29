6e9ad55bb0f11df4edb53131afd2df352443c762
AkshatDobriyal <201951016@iiitvadodara.ac.in>
2022-01-29 13:27:19 +0530

    Feat: Added product tracking page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import SideNav from './components/sideNav/SideNav'
 import CreateEntity from './pages/entity/CreateEntity'
+import ProductTracking from './pages/tracking/ProductTracking'
 function App() {
   return (
     <div>
       <Router>
-      <SideNav/>
-      <CreateEntity/>
-        <Switch>
-          {/* <Route exact path="/" component={Home}/> */}
-        </Switch>
+        <SideNav/>
+          <Switch>
+          <Route exact path="/createEntity" component={CreateEntity} />
+            <Route exact path="/tracking" component={ProductTracking}/>
+          </Switch>
       </Router>
 
     </div>
---
+++ src/pages/tracking/ProductTracking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/header/Header.js';
+
+function ProductTracking() {
+    return (
+        <div>
+            <Header title='Product Tracking' />
+        </div>
+    );
+}
+
+export default ProductTracking;
