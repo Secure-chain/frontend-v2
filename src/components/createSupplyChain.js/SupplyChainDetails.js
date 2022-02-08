59e43564ce3ff3ab14fb3f73057689347edf7b70
siddhantsingh186 <201951151@iiitvadodara.ac.in>
2022-02-08 19:33:16 +0530

    minor fixes

--- src/components/createSupplyChain.js/SupplyChainDetails.js
+++ src/components/createSupplyChain.js/SupplyChainDetails.js
@@ -20,7 +20,7 @@ function SupplyChainDetails({setId,handleTabChange}) {
       <div className='supply-chain-header'>
         Supply Chain Details
       </div>
-      <div className='input-container'>
+      <div className='supply-input-container'>
         <div className='input-label'>
           Name
         </div>
