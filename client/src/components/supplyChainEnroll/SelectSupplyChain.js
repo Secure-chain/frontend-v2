030a929c7f769e5026fe1a6858c0a0e14aebb9b0
AkshatDobriyal <201951016@iiitvadodara.ac.in>
2022-02-09 00:26:23 +0530

    Feat: Added Select component for selecting supply chain

--- client/src/components/supplyChainEnroll/SelectSupplyChain.js
+++ client/src/components/supplyChainEnroll/SelectSupplyChain.js
@@ -1,10 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import selectSupplyChain from './selectSupplyChain.css'
+//import SupplyChainPhoto from '../../../public/media/form2.png'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import selectSupplyChain from './selectSupplyChain.css';
 
 function SelectSupplyChain({ handleTabChange }) {
     return(
-        <div>
-
+        <div className='supply-chain'>
+            <div className='picture'>
+                <img src='../../../public/media/form2.png' alt='' />
+            </div>
+            <div className='content'>
+                <div className='select-text'>
+                    Choose from the following existing supply chains
+                </div>
+                <div className='select-box'>
+                    <Box sx={{ minWidth: 300 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Choose</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value=""
+                                label="Age"
+                                onChange=""
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </div>
+            </div>
         </div>
     );
 }
