28b029f1bbacb2b138e5dea2a392ffeed9a8bea9
AkshatDobriyal <201951016@iiitvadodara.ac.in>
2022-02-14 14:17:54 +0530

    Feat: Added feature to transfer batches of a product

--- client/src/pages/transfer/TransferProduct.js
+++ client/src/pages/transfer/TransferProduct.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from 'react'
 import TransferProductContent from '../../components/transferProduct/TransferProductContent'
 import Header from '../../components/header/Header'
 
-function TransferProduct() {
+function TransferProduct({ getProductName, productsInSupplyChain, currentBatchesInOwnership, currentUnitsInOwnership, transferProduct, requestTransfer }) {
     return (
         <div>
             <Header title='Transfer Product' />
-            <TransferProductContent />
+            <TransferProductContent
+                getProductName={getProductName}
+                productsInSupplyChain={productsInSupplyChain}
+                currentBatchesInOwnership={currentBatchesInOwnership}
+                currentUnitsInOwnership={currentUnitsInOwnership}
+                transferProduct={transferProduct}
+                requestTransfer={requestTransfer}
+            />
         </div>
     );
 }
