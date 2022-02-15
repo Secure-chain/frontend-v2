8cfcc756ef20da44f6ad43aeb81229b083bcf0c2
AkshatDobriyal <201951016@iiitvadodara.ac.in>
2022-02-16 01:13:49 +0530

    Feat: Added feature to backtrack the travel history of a particular batch of a product

--- client/src/components/trackingContent/ProductHistory.js
+++ client/src/components/trackingContent/ProductHistory.js
@@ -1,18 +1,50 @@
 import React, { useState, useEffect } from 'react'
+import line from '../../media/line.png'
 import productHistory from './productHistory.css'
 
 function ProductHistory({ batchHistory}) {
 
     return (
         <div className='product-history'>
-            <div className='product-history-container'>
-                <div className='product-history-date'>
-                    Monday, 20 December, 2021
+            {
+                batchHistory &&
+                <div className='product-history-container'>
+                    {
+                        batchHistory.map((e) => {
+                            let block;
+                            if (e.description === "Transfer Requested") {
+                                block = e.description + " by " + e._senderName + " to " + e._receiverName;
+                            }
+                            else if (e.description === "Product Created") {
+                                block = e.description + " by " + e._senderName;
+                            }
+                            else if (e.description === "Transfer Request Accepted") {
+                                block = e.description + " by " + e._receiverName;
+                            }
+                            else {
+                                block = e.description + " to " + e._receiverName;
+                            }
+                            return (
+                                <div className='tracking-content' key={e.timestamp}>
+                                    <div className='line' style={{}}>
+                                        
+                                    </div>
+                                    <div>
+                                        <div className='product-history-date'>
+                                            {e.timestamp}
+                                        </div>
+                                        <div className='product-history-info'>
+                                            {block}
+                                            <br/>
+                                            Number of units:- {e.unitsInBatch}
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
                 </div>
-                <div className='product-history-info'>
-                    Krishna Manufacturer manufactured 20 batches of Covishield
-                </div>
-            </div>
+            }
         </div>
     );
 }
