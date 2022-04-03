7271709fa2711b3052f3c25770811f302dd1b717
Krishna Gaur <gaurkrishna498@gmail.com>
2022-04-04 01:56:27 +0530

    optimal APi integrated

--- client/src/components/initVariables/initObject.js
+++ client/src/components/initVariables/initObject.js
@@ -1,6 +1,6 @@
 export const initObject = () => {
     return {
-    initVariables : "http://127.0.0.1:8000",
+    initVariables : "http://securechain-backend.herokuapp.com/",
     token: localStorage.getItem("token"),
     }
 }
